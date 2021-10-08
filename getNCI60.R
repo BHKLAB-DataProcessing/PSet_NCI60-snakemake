d6b4aefcfab0ffc23edd8645d2554fd51de95b31
Minoru Nakano <minoru.nakano@gmail.com>
2021-10-08 12:30:31 -0400

    troubleshooting

--- getNCI60.R
+++ getNCI60.R
@@ -21,8 +21,8 @@ drug_with_ids <- read.csv(paste0(annotation_dir,"drugs_with_ids.csv"), stringsAs
 cell<-read.delim(paste0(cell_dir, "NCI60_CELL_LINE_METADATA.txt") , skip = 7, check.names =F)[c(1:60),]
 rna <- read_excel(paste0(mol_dir, "RNA__Affy_HG_U133_Plus_2.0_RMA.xls") , skip =10)
 # mirna <- read_excel(paste0(mol_dir, "RNA__Agilent_Human_microRNA_(V2)_GeneSpringGX.xls") ,skip = 10)
-# rnaseq.comp <- read_excel(paste0(mol_dir, "RNA__RNA_seq_composite_expression.xls") ,skip = 10)
-# rnaseq.iso <- read_excel(paste0(mol_dir, "RNA__RNA_seq_isoforms.xls") ,skip = 10)
+rnaseq.comp <- read_excel(paste0(mol_dir, "RNA__RNA_seq_composite_expression.xls") ,skip = 10)
+rnaseq.iso <- read_excel(paste0(mol_dir, "RNA__RNA_seq_isoforms.xls") ,skip = 10)
 dose.resp<-fread(paste0(sens_dir,"DOSERESP.csv"))
 raw.sensitivity <-readRDS(paste0(sens_dir,"raw.sensitivity_v3.rds"))
 profile.sensitivity <-readRDS(paste0(sens_dir,"profile.sensitivity_v3.rds"))
